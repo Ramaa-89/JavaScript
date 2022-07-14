f8c75188b88791f885c5c64635b33f1463dde014
Ramaa-89 <64108130+Ramaa-89@users.noreply.github.com>
2022-07-14 20:16:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,158 +11,83 @@ class Producto {
     this.cantidad = 0;
   }
 
-  vender(cantida) {
-    precioTotal = suma(precioTotal, cantida * this.precio);
-    this.cantidad = this.cantidad + cantida;
+  vender(cantidad) {
+    precioTotal = suma(precioTotal, cantidad * this.precio);
+    this.cantidad = this.cantidad + cantidad;
   }
 }
 
 //Productos
-const producto1 = new Producto("Royal Canin gato - Strelized x 7kg", 3539.99);
-const producto2 = new Producto("ProPlan gato - Adulto x7kg", 3249.99);
-const producto3 = new Producto("Royal Canin perro - Adulto x 10kg", 3500);
-const producto4 = new Producto("ProPlan perro - Adulto x10kg", 3200);
-const producto5 = new Producto("Royal Canin gato - Kitten x 7kg", 3249.99);
-const producto6 = new Producto("ProPlan gato - Kitten x7kg", 3149.99);
-const producto7 = new Producto("Royal Canin - Alimento humedo - control 90gr", 245);
-const producto8 = new Producto("Royal Canin - Alimento humedo - agile 90gr", 245);
-const producto9 = new Producto("Royal Canin - Alimento humedo - adult 90gr", 245);
-const producto10 = new Producto("Royal Canin - Alimento humedo - smell 90gr", 245);
-const producto11 = new Producto("ProPlan - Alimento humedo - control 90gr", 215);
-const producto12 = new Producto("ProPlan - Alimento humedo - agile 90gr", 215);
-const producto13 = new Producto("ProPlan - Alimento humedo - adult 90gr", 215);
-const producto14 = new Producto("ProPlan - Alimento humedo - kitten 90gr", 215);
+const productos = [
+  new Producto("Royal Canin gato - Strelized x 7kg", 3539.99),
+  new Producto("ProPlan gato - Adulto x7kg", 3249.99),
+  new Producto("Royal Canin perro - Adulto x 10kg", 3500),
+  new Producto("ProPlan perro - Adulto x10kg", 3200),
+  new Producto("Royal Canin gato - Kitten x 7kg", 3249.99),
+  new Producto("ProPlan gato - Kitten x7kg", 3149.99),
+  new Producto("Royal Canin - Alimento humedo - control 90gr", 245),
+  new Producto("Royal Canin - Alimento humedo - agile 90gr", 245),
+  new Producto("Royal Canin - Alimento humedo - adult 90gr", 245),
+  new Producto("Royal Canin - Alimento humedo - smell 90gr", 245),
+  new Producto("ProPlan - Alimento humedo - control 90gr", 215),
+  new Producto("ProPlan - Alimento humedo - agile 90gr", 215),
+  new Producto("ProPlan - Alimento humedo - adult 90gr", 215),
+  new Producto("ProPlan - Alimento humedo - kitten 90gr", 215),
+];
+
+console.log(productos);
 
 alert(
   "Gracias por venir a goPet!\nElegí lo mejor para tu mascota!"
 );
 
-let eleccionProducto = parseInt(
-  prompt(`Seleccioná el producto:
-    (1) ${producto1.nombre} 
-    (2) ${producto2.nombre}
-    (3) ${producto3.nombre}
-    (4) ${producto4.nombre}
-    (5) ${producto5.nombre}
-    (6) ${producto6.nombre}
-    (7) ${producto7.nombre}
-    (8) ${producto8.nombre}
-    (9) ${producto9.nombre}
-    (10) ${producto10.nombre}
-    (11) ${producto11.nombre}
-    (12) ${producto12.nombre}
-    (13) ${producto13.nombre}
-    (14) ${producto14.nombre}
-    (0) No quiero comprar.`)
-);
+const nombres = [
+  "Seleccioná el producto:",
+  ...productos.map((prod, i) => `(${i + 1}) ${prod.nombre}`),
+  "(0) Cancelar compra.",
+];
 
-while (eleccionProducto !== 0) {
-  let cantidad = parseInt(prompt("¿Cuantas unidades querés agregar?"));
-  if (isNaN(cantidad)) {
-    alert("Ingresá una cantidad válida");
-  } else {
-    if (eleccionProducto === 1) {
-      producto1.vender(cantidad);
-    } else if (eleccionProducto === 2) {
-      producto2.vender(cantidad);
-    } else if (eleccionProducto === 3) {
-      producto3.vender(cantidad);
-    } else if (eleccionProducto === 4) {
-      producto4.vender(cantidad);
-    } else if (eleccionProducto === 5) {
-      producto5.vender(cantidad);
-    } else if (eleccionProducto === 6) {
-      producto6.vender(cantidad);
-    } else if (eleccionProducto === 7) {
-      producto7.vender(cantidad);
-    } else if (eleccionProducto === 8) {
-      producto8.vender(cantidad);
-    } else if (eleccionProducto === 9) {
-      producto9.vender(cantidad);
-    } else if (eleccionProducto === 10) {
-      producto10.vender(cantidad);
-    } else if (eleccionProducto === 11) {
-      producto11.vender(cantidad);
-    } else if (eleccionProducto === 12) {
-      producto12.vender(cantidad);
-    } else if (eleccionProducto === 13) {
-      producto13.vender(cantidad);
-    } else if (eleccionProducto === 14) {
-      producto14.vender(cantidad);
-    } else {
-      alert("Ingresá una opción válida");
-    }
-  }
-  
-  let seguirComprando = prompt("¿Querés comprar algo más?").toLowerCase();
-
-  while (seguirComprando === "si") {
-    let eleccionProducto = parseInt(
-      prompt(`Seleccioná el producto:
-      (1) ${producto1.nombre} 
-      (2) ${producto2.nombre}
-      (3) ${producto3.nombre}
-      (4) ${producto4.nombre}
-      (5) ${producto5.nombre}
-      (6) ${producto6.nombre}
-      (7) ${producto7.nombre}
-      (8) ${producto8.nombre}
-      (9) ${producto9.nombre}
-      (10) ${producto10.nombre}
-      (11) ${producto11.nombre}
-      (12) ${producto12.nombre}
-      (13) ${producto13.nombre}
-      (14) ${producto14.nombre}
-      `)
-    );
-    cantidad = parseInt(prompt("¿Cuantas unidades querés agregar?"));
-    if (isNaN(cantidad)) {
-      alert("Ingresar una cantidad válida");
-    } else {
-      if (eleccionProducto === 0) {
-        alert(
-            "Gracias por visitarnos\nEn goPet estamos para lo que necesite tu mejor amigo\nEperamos volver a verte pronto."
-        );
-        break;
-      } else if (eleccionProducto === 1) {
-        producto1.vender(cantidad);
-      } else if (eleccionProducto === 2) {
-        producto2.vender(cantidad);
-      } else if (eleccionProducto === 3) {
-        producto3.vender(cantidad);
-      } else if (eleccionProducto === 4) {
-        producto4.vender(cantidad);
-      } else if (eleccionProducto === 5) {
-        producto5.vender(cantidad);
-      } else if (eleccionProducto === 6) {
-        producto6.vender(cantidad);
-      } else if (eleccionProducto === 7) {
-        producto7.vender(cantidad);
-      } else if (eleccionProducto === 8) {
-        producto8.vender(cantidad);
-      } else if (eleccionProducto === 9) {
-        producto9.vender(cantidad);
-      } else if (eleccionProducto === 10) {
-        producto10.vender(cantidad);
-      } else if (eleccionProducto === 11) {
-        producto11.vender(cantidad);
-      } else if (eleccionProducto === 12) {
-        producto12.vender(cantidad);
-      } else if (eleccionProducto === 13) {
-        producto13.vender(cantidad);
-      } else if (eleccionProducto === 14) {
-        producto14.vender(cantidad);
+function cargandoProductos() {
+  let productoElegido = parseInt(prompt(nombres.join("\n")));
+  let continuaCompra = true;
+  while (productoElegido !== 0 && continuaCompra) {
+    
+    if (productoElegido >= 1 && productoElegido <= productos.length) {
+      let cantidad = parseInt(prompt("¿Cuantas unidades querés agregar?"));
+      if (isNaN(cantidad)) {
+        alert("Ingresá una cantidad válida");
       } else {
-        alert("Ingresá una opcion válida");
+        productos[productoElegido - 1].vender(cantidad);
       }
+    } else {
+      alert("Ingresá una opcion válida");
     }
-    seguirComprando = prompt("¿Necesitás agregar algo más al carrito?").toLowerCase();
+    continuaCompra = confirm("¿Necesitás agregar algo más al carrito?");
+   
+    if (continuaCompra) {
+      return cargandoProductos();
+    }
   }
+
+  if (precioTotal != 0) {
+    
+    let listaVendidos = productos
+    .filter((prod) => prod.cantidad > 0)
+    .map((prod) => `• ${prod.cantidad} ${prod.nombre}`)
+    .join("\n");
 
   alert(
-    `El total a pagar es de $ ${precioTotal}.\n¡Muchas Gracias por confiar en nosotros!`
+    `Vas a comprar: \n${listaVendidos} \n\nEl total a pagar es de $ ${precioTotal}.\n\n¡Muchas Gracias por confiar en nosotros!`
   );
-  break;
+  }
+
+  alert("Gracias por visitarnos\nEn goPet estamos para lo que necesite tu mejor amigos\n\nEperamos volver a verte pronto.");
 }
 
-alert("Gracias por visitarnos\nEn goPet estamos para lo que necesite tu mejor amigo\nEperamos volver a verte pronto.");
+cargandoProductos();
+
+console.log(precioTotal);
+productos
+  .filter((prod) => prod.cantidad > 0)
+  .map((prod) => `Se vendieron ${prod.cantidad} ${prod.nombre}`)
+  .forEach((resultadoVentaProd) => console.log(resultadoVentaProd));
